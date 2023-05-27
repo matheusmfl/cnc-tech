68f23172244da487bcd3ace36bc6053a403e639a
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-05-27 18:45:56 -0300

    fix: Last Commit Error Fixed

--- src/components/home/section4/Section4.tsx
+++ src/components/home/section4/Section4.tsx
@@ -2,7 +2,7 @@ import { NextButton } from '../Button'
 
 export function Section4() {
   return (
-    <section className="p-10 flex flex-col bg-slate-100 gap-7 border-white">
+    <section className="p-10 flex flex-col bg-slate-100 gap-7">
       <div className="flex flex-col gap-3">
         <h2 className="title leading-7 text-black">
           <strong>
@@ -14,7 +14,7 @@ export function Section4() {
           Conheça nossos cursos para equipes de alto desempenho
         </span>
       </div>
-      <div className="flex justify-end w-full border-black">
+      <div className="flex justify-end w-full border-white">
         <NextButton />
       </div>
     </section>
