79325295a0b8b45bb3e36a3c4263577e88ff7165
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-05-27 15:15:30 -0300

    Feat : Add icons on MenuItems, create MenuItems Interface

--- src/components/home/CollapsibleComponent.tsx
+++ src/components/home/CollapsibleComponent.tsx
@@ -2,19 +2,34 @@
 'use client'
 import * as Collapsible from '@radix-ui/react-collapsible'
 import logo from '../../assets/logo.svg'
-import { Search, Menu, X, Package } from 'lucide-react'
+import {
+  Search,
+  Menu,
+  X,
+  Package,
+  Settings,
+  UserSquare2,
+  Info,
+} from 'lucide-react'
 import Image from 'next/image'
 import { useState } from 'react'
 import iconMore from '../../assets/iconMore.svg'
 
 // Items do menu
+interface MenuItems {
+  icon: 'package' | 'settings' | 'contact' | 'info'
+  title: string
+}
 
-function MenuItem() {
+function MenuItem({ icon, title }: MenuItems) {
   return (
-    <div className="border-b border-solid border-slate-400 flex py-4 px-5 items-center justify-between bg-slate-100">
+    <div className="border-b border-solid text-gray-700 border-slate-400 flex py-4 px-5 items-center justify-between bg-slate-100">
       <div className="flex gap-[22px]">
-        <Package />
-        <span>Produtos</span>
+        {icon === 'package' && <Package />}
+        {icon === 'contact' && <UserSquare2 />}
+        {icon === 'info' && <Info />}
+        {icon === 'settings' && <Settings />}
+        <span>{title}</span>
       </div>
       <button>
         <Image
@@ -32,7 +47,7 @@ export function CollapsibleComponent() {
   const [open, setOpen] = useState(false)
   return (
     <Collapsible.Root open={open} onOpenChange={setOpen}>
-      <header className="bg-slate-100 flex justify-between p-5 items-center border-b border-solid border-slate-400">
+      <header className="bg-slate-100 color-gray-700 leading-5 flex justify-between p-5 items-center border-b border-solid border-slate-400">
         <Search size={20} />
         <Image src={logo} alt="Logo" />
         <Collapsible.Trigger asChild>
@@ -40,7 +55,7 @@ export function CollapsibleComponent() {
         </Collapsible.Trigger>
       </header>
       <Collapsible.Content>
-        <MenuItem />
+        <MenuItem title="Produtos" icon="package" />
       </Collapsible.Content>
     </Collapsible.Root>
   )
