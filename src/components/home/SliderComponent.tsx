19ecd4bea3047c7e969cd3c968ca45d3b3453174
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-05-27 21:12:37 -0300

    Create SwiperPagination

--- src/components/home/SliderComponent.tsx
+++ src/components/home/SliderComponent.tsx
@@ -1,18 +1,22 @@
 'use client'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination } from 'swiper'
 
 import 'swiper/css'
-import 'swiper/css/scrollbar'
+import 'swiper/css/pagination'
 import { BlogCard } from './BlogCard'
 
 export function SliderComponent() {
   return (
     <div className=" bg-slate-200 rounded-3xl shadow-xl">
       <Swiper
+        scrollbar={{ el: '.swiper-scrollbar', draggable: true, enabled: true }}
+        pagination={{ enabled: true, clickable: true }}
         spaceBetween={50}
         slidesPerView={1}
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
+        modules={[Pagination]}
       >
         <SwiperSlide>
           <BlogCard />
@@ -27,6 +31,7 @@ export function SliderComponent() {
           <BlogCard />
         </SwiperSlide>
       </Swiper>
+      <div className="swiper-pagination"></div>
     </div>
   )
 }
