21711d170777328c82befb328f77daf9c5f0a149
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-05-28 21:39:27 -0300

    feat: Create section 11

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Header } from '@/components/home/header/Header'
 import { HeroSection } from '@/components/home/heroSection/HeroSection'
 import { Section1 } from '@/components/home/section1/Section1'
 import { Section10 } from '@/components/home/section10/Section10'
+import { Section11 } from '@/components/home/section11/Section11'
 import { Section2 } from '@/components/home/section2/Section2'
 import { Section3 } from '@/components/home/section3/Section3'
 import { Section4 } from '@/components/home/section4/Section4'
@@ -28,6 +29,7 @@ export default function Home() {
       <SectionFeedbacks />
       <Section9 />
       <Section10 />
+      <Section11 />
     </div>
   )
 }
---
+++ src/components/home/section11/Section11.tsx
@@ -0,0 +1,25 @@
+import { Button } from '../Button'
+import { SectionNameComponent } from '../SectionNameComponent'
+
+export function Section11() {
+  return (
+    <section className="flex flex-col gap-16 px-5 py-10 bg-gray-300">
+      {/* Nome da Sessão */}
+      <SectionNameComponent>
+        <h2 className="title text-black">
+          <strong>Converse</strong> com um representante{' '}
+          <strong>CNCTech</strong>
+        </h2>
+      </SectionNameComponent>
+
+      <span className="font-normal text-base leading-6 text-slate-900">
+        Tire suas dúvidas com um dos nossos representantes no Brasil, América do
+        Sul e Estados Unidos.
+      </span>
+
+      <Button>
+        <strong>Falar com representante</strong>
+      </Button>
+    </section>
+  )
+}
