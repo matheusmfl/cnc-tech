a3aa0a87f369ce01548a9a83f116d4f1242de3c3
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-06-02 16:36:08 -0300

    Edit section1

--- src/components/home/section1/Section1.tsx
+++ src/components/home/section1/Section1.tsx
@@ -2,7 +2,8 @@ import { Button } from '../Button'
 
 export function Section1() {
   return (
-    <section className="flex flex-col py-10 px-5 gap-5 bg-slate-100">
+    <section className="flex flex-col py-10 px-5 gap-5 section-white">
+      <div className="section-white-before"></div>
       <span className="text-lg text-slate-800 font-medium w-[310px]">
         Fale com nossos representantes e veja as melhores soluções pro seu
         negócio.
