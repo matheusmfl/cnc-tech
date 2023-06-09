4f3acb1dbb9c2792535cb468c2a526203d9049d2
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-06-08 21:50:56 -0300

    fix ProjectId Bug for deploy

--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -1,5 +1,5 @@
 export const clientConfig = {
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: 'production',
   apiVersion: '2023-03-06',
 }
