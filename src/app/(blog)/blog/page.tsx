6233156d66fa902514111023278742eac69e9c82
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-06-08 21:47:47 -0300

    Fix bug TSExpectError

--- src/app/(blog)/blog/page.tsx
+++ src/app/(blog)/blog/page.tsx
@@ -8,7 +8,7 @@ export default async function BlogHome() {
       <SearchComponent />
       {/* @ts-expect-error */}
       <SectionDestaques />
-      {/* @ts-expect-error */}
+
       <SectionFeed />
     </>
   )
