403bede0d96516188105b20cd553314f8a1024ba
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-09-09 19:53:29 -0300

    update section representante comercial, add mapa-mundi background

--- src/components/home/section11/Section11.tsx
+++ src/components/home/section11/Section11.tsx
@@ -1,8 +1,6 @@
-import { Button } from '../Button'
-
 export function Section11() {
   return (
-    <section className="flex flex-col gap-16 px-5 py-10 bg-gray-300">
+    <section className="flex flex-col gap-16 px-5 py-10 bg-slate-100 bg-mapa-mundi bg-auto bg-center bg-no-repeat">
       {/* Nome da Sessão */}
 
       <h2 className="text-2xl font-roboto font-medium leading-[32px] text-slate-800">
@@ -10,14 +8,16 @@ export function Section11() {
         <strong>CNCTech</strong>
       </h2>
 
-      <span className="font-normal text-base leading-6 text-slate-900">
+      <span className="font-medium font-roboto text-base leading-6 text-slate-900">
         Tire suas dúvidas com um dos nossos representantes no Brasil, América do
         Sul e Estados Unidos.
       </span>
 
-      <Button>
-        <strong>Falar com representante</strong>
-      </Button>
+      <a href="#" className="w-full">
+        <button className="py-3 rounded-lg text-center font-roboto font-medium text-sm text-slate-50 bg-[#1F2A68] w-full">
+          Falar com representante &gt;
+        </button>
+      </a>
     </section>
   )
 }
