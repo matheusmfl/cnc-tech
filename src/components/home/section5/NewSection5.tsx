0acbed1d94d7b4c9f120718745b5a7988b4da87b
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-09-14 19:15:47 -0300

    update section5 Add Desktop Version

--- src/components/home/section5/NewSection5.tsx
+++ src/components/home/section5/NewSection5.tsx
@@ -5,82 +5,85 @@ import img3 from '../../../assets/cursosImage.jpeg'
 
 export function NewSection5() {
   return (
-    <section className="bg-slate-200 py-10 px-5 flex flex-col gap-5 ">
+    <section className="bg-slate-200 py-10 px-5 flex flex-col gap-5 md:py-10 md:px-20">
       <h2 className="text-2xl font-roboto font-medium leading-[32px] text-slate-800">
         Contato
       </h2>
 
-      {/* Primeiro CARD */}
+      {/* container de cards */}
+      <div className="md:grid md:grid-cols-3 md:gap-5">
+        {/* Primeiro CARD */}
 
-      <div className="flex flex-col rounded-md shadow-lg">
-        <Image
-          src={img1}
-          alt="Suporte e atendimento CNC-Tech"
-          className="aspect-video rounded-t-lg"
-        />
+        <div className="flex flex-col rounded-md shadow-lg md:flex-col-reverse">
+          <Image
+            src={img1}
+            alt="Suporte e atendimento CNC-Tech"
+            className="aspect-video rounded-t-lg md:h-[240px] md:object-cover md:rounded-t-none"
+          />
 
-        <div className="p-5 flex flex-col gap-5">
-          <h3 className="font-roboto font-medium text-lg leading-[24px] text-sky-900">
-            Atendimento e suporte{' '}
-          </h3>
+          <div className="p-5 flex flex-col gap-5 md:bg-slate-50 md:h-[163px] md:justify-between">
+            <h3 className="font-roboto font-medium text-lg leading-[24px] text-sky-900">
+              Atendimento e suporte{' '}
+            </h3>
 
-          <div className="pt-[17px] flex w-full items-end justify-end">
-            <a
-              href="/"
-              className="text-[#1F2A68] font-medium text-base leading-[24px]"
-            >
-              Suporte CNCTech &gt;&gt;{' '}
-            </a>
+            <div className="pt-[17px] flex w-full items-end justify-end">
+              <a
+                href="/"
+                className="text-[#1F2A68] font-medium text-base leading-[24px]"
+              >
+                Suporte CNCTech &gt;&gt;{' '}
+              </a>
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* Segundo CARD */}
+        {/* Segundo CARD */}
 
-      <div className="flex flex-col rounded-md shadow-lg">
-        <Image
-          src={img2}
-          alt="Suporte e atendimento CNC-Tech"
-          className="aspect-video rounded-t-lg object-cover"
-        />
+        <div className="flex flex-col rounded-md shadow-lg md:flex-col-reverse">
+          <Image
+            src={img2}
+            alt="Suporte e atendimento CNC-Tech"
+            className="aspect-video rounded-t-lg md:h-[240px] md:object-cover md:rounded-t-none"
+          />
 
-        <div className="p-5 flex flex-col gap-5">
-          <h3 className="font-roboto font-medium text-lg leading-[24px] text-sky-900">
-            Representante, encontre um próximo a você{' '}
-          </h3>
+          <div className="p-5 flex flex-col gap-5 md:bg-slate-50 md:h-[163px] md:justify-between">
+            <h3 className="font-roboto font-medium text-lg leading-[24px] text-sky-900">
+              Representante, encontre um próximo a você{' '}
+            </h3>
 
-          <div className="pt-[17px] flex w-full items-end justify-end">
-            <a
-              href="/"
-              className="text-[#1F2A68] font-medium text-base leading-[24px]"
-            >
-              &gt;&gt;{' '}
-            </a>
+            <div className="pt-[17px] flex w-full items-end justify-end">
+              <a
+                href="/"
+                className="text-[#1F2A68] font-medium text-base leading-[24px]"
+              >
+                &gt;&gt;{' '}
+              </a>
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* Terceiro CARD */}
+        {/* Terceiro CARD */}
 
-      <div className="flex flex-col rounded-md shadow-lg">
-        <Image
-          src={img3}
-          alt="Suporte e atendimento CNC-Tech"
-          className="aspect-video rounded-t-lg object-cover"
-        />
+        <div className="flex flex-col rounded-md shadow-lg md:flex-col-reverse">
+          <Image
+            src={img3}
+            alt="Suporte e atendimento CNC-Tech"
+            className="aspect-video rounded-t-lg md:h-[240px] md:object-cover md:rounded-t-none"
+          />
 
-        <div className="p-5 flex flex-col gap-5">
-          <h3 className="font-roboto font-medium text-lg leading-[24px] text-sky-900">
-            Cursos de capacitação de equipes{' '}
-          </h3>
+          <div className="p-5 flex flex-col gap-5 md:bg-slate-50 md:h-[163px] md:justify-between">
+            <h3 className="font-roboto font-medium text-lg leading-[24px] text-sky-900">
+              Cursos de capacitação de equipes{' '}
+            </h3>
 
-          <div className="pt-[17px] flex w-full items-end justify-end">
-            <a
-              href="/"
-              className="text-[#1F2A68] font-medium text-base leading-[24px]"
-            >
-              Ver cursos &gt;&gt;{' '}
-            </a>
+            <div className="pt-[17px] flex w-full items-end justify-end">
+              <a
+                href="/"
+                className="text-[#1F2A68] font-medium text-base leading-[24px]"
+              >
+                Ver cursos &gt;&gt;{' '}
+              </a>
+            </div>
           </div>
         </div>
       </div>
