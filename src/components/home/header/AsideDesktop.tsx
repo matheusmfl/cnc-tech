120860b31e8c1b04883ac8d55729c71f74830436
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-09-14 20:19:45 -0300

    update asideDesktop border

--- src/components/home/header/AsideDesktop.tsx
+++ src/components/home/header/AsideDesktop.tsx
@@ -5,7 +5,7 @@ export function AsideDesktop() {
   return (
     <aside className="hidden md:flex md:flex-col  w-[320px] ">
       {/* Search div */}{' '}
-      <div className="border bg-slate-100 cursor-pointer border-[#1F2A68] px-[25px] py-[30px] w-full flex items-center justify-center gap-[10px] h-[80px]">
+      <div className="border-b bg-slate-100 cursor-pointer border-[#1F2A68] px-[25px] py-[30px] w-full flex items-center justify-center gap-[10px] h-[80px]">
         <Search size={20} className="text-[#1F2A68]" />
         <span className="uppercase text-base font-roboto font-medium leading-[20px] text-[#1F2A68]">
           pesquisa
