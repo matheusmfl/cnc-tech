33d970c303be44f16c238769a2bf5eecd3ca99c2
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-09-14 20:35:12 -0300

    ending footer desktop version

--- src/components/home/footer/Footer.tsx
+++ src/components/home/footer/Footer.tsx
@@ -9,49 +9,58 @@ import {
 
 export function Footer() {
   return (
-    <footer className="flex flex-col py-10 px-5 gap-10 bg-slate-800">
+    <footer className="flex flex-col py-10 px-5 gap-10 bg-slate-800 md:px-20">
       {/* Logo do Footer */}
       <div className="flex items-center justify-center py-3">
         <Image src={logoFooter} alt="Logo" />
       </div>
+
       {/* Lista de navegação do footer */}
-      <div>
-        <nav className="list-none text-slate-50 text-xs flex flex-col gap-5">
-          <span className="text-sm">Contato</span>
-          <ul className="flex flex-col gap-2">
-            <li className="cursor-pointer hover:underline">
-              +55 (54) 99112 - 3338
-            </li>
-            <li className="cursor-pointer hover:underline">
-              +55 (54) 98105 - 4365
-            </li>
-          </ul>
 
-          <span className="text-sm">Sobre nós</span>
-          <ul className="flex flex-col gap-2">
-            <li className="cursor-pointer hover:underline">
-              Quem somos (história e valores)
-            </li>
-            <li className="cursor-pointer hover:underline">
-              Membros e parceiros
-            </li>
-          </ul>
+      <nav className="list-none text-slate-50 text-xs flex flex-col gap-5 md:w-full">
+        <div className="flex flex-col md:flex-row gap-5 md:justify-between md:w-full">
+          <div className="flex flex-col gap-5">
+            <span className="text-sm">Contato</span>
+            <ul className="flex flex-col gap-2">
+              <li className="cursor-pointer hover:underline">
+                +55 (54) 99112 - 3338
+              </li>
+              <li className="cursor-pointer hover:underline">
+                +55 (54) 98105 - 4365
+              </li>
+            </ul>
+          </div>
 
-          <span className="text-sm">Mais</span>
-          <ul className="flex flex-col gap-2">
-            <li className="cursor-pointer hover:underline">
-              Quer fazer parte da equipe?
-            </li>
-            <li className="cursor-pointer hover:underline">
-              Novidades CNCTech
-            </li>
-          </ul>
+          <div className="flex flex-col gap-5">
+            <span className="text-sm">Sobre nós</span>
+            <ul className="flex flex-col gap-2">
+              <li className="cursor-pointer hover:underline">
+                Quem somos (história e valores)
+              </li>
+              <li className="cursor-pointer hover:underline">
+                Membros e parceiros
+              </li>
+            </ul>
+          </div>
 
-          <span className="pt-5 cursor-pointer hover:underline">
-            R. Armando Marco Zanetti, 09 - Tamandaré - Garibaldi, RS/Brasil
-          </span>
-        </nav>
-      </div>
+          <div className="flex flex-col gap-5">
+            <span className="text-sm">Mais</span>
+            <ul className="flex flex-col gap-2">
+              <li className="cursor-pointer hover:underline">
+                Quer fazer parte da equipe?
+              </li>
+              <li className="cursor-pointer hover:underline">
+                Novidades CNCTech
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <span className="pt-5 cursor-pointer hover:underline md:text-center md:w-full">
+          R. Armando Marco Zanetti, 09 - Tamandaré - Garibaldi, RS/Brasil
+        </span>
+      </nav>
+
       {/* Links sociais do footer */}
       <div className="flex justify-between text-slate-50">
         <a href="/">
