5236d4d69ceec27f7e845a94960004ecc9595dea
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-09-14 19:05:15 -0300

    update section 4.5Middle

--- src/components/home/section4.5/Section4middle.tsx
+++ src/components/home/section4.5/Section4middle.tsx
@@ -4,25 +4,29 @@ import image from '../../../assets/servicosCncTech.jpeg'
 
 export function Section4Middle() {
   return (
-    <section className="bg-[#F0F2F4] py-10 px-5 flex flex-col gap-5 rounded-lg overflow-hidden">
+    <section className="bg-[#F0F2F4] py-10 px-5 flex flex-col gap-5 rounded-lg overflow-hidden md:px-20">
       {/* logo div */}
       <div className="flex items-center justify-center">
-        <Image src={logo} alt="nossa logomarca" className="w-[141px]" />
+        <Image src={logo} alt="nossa logomarca" className="w-[141px] " />
       </div>
 
       {/* Card */}
-      <div className="flex flex-col shadow-lg rounded-b-lg ">
+      <div className="flex flex-col md:flex-row shadow-lg rounded-b-lg ">
         {/* img div */}
         <div>
-          <Image src={image} alt="Imagem generica" className="rounded-t-lg" />
+          <Image
+            src={image}
+            alt="Imagem generica"
+            className="rounded-t-lg md:w-[640px] md:rounded-l-lg md:scale-105 md:rounded-r-none"
+          />
         </div>
         {/* div abaixo imagem */}
-        <div className="flex flex-col p-5 gap-4">
-          <h2 className="font-roboto font-medium text-[#1F2A68] leading-[24px] text-lg">
+        <div className="flex flex-col p-5 gap-4 md:py-10">
+          <h2 className="font-roboto font-medium text-[#1F2A68] leading-[24px] text-lg md:text-[24px]">
             Conheça um pouco mais da nossa história
           </h2>
 
-          <span className="font-roboto font-medium text-[#3C90CE] leading-[24px] text-base">
+          <span className="font-roboto font-medium text-[#3C90CE] leading-[24px] text-base md:text-[18px]">
             e do que faz a CNCTech a melhor escolha para atender você e o seu
             negócio.
           </span>
