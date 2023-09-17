da16dea67ad9e03210d42c7ae26ce0a17ad73496
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-09-17 15:00:28 -0300

    Create Product Card, ajust the folders on Product Router

---
+++ src/app/(product)/product/page.tsx
@@ -0,0 +1,25 @@
+import { AsideDesktop } from '@/components/home/header/AsideDesktop'
+import { Header } from '@/components/home/header/Header'
+
+export default function Product() {
+  return (
+    <>
+      <main className="flex">
+        <div className="md:fixed md:left-0 z-10">
+          <Header />
+          <AsideDesktop />
+        </div>
+
+        {/* Container principal */}
+        <section className="flex flex-col gap-5 z-0 md:ml-[320px]">
+          <h2 className="font-roboto font-medium text-[#1F2A68] leading-[24px] text-lg md:text-[24px]">
+            Centro de furação
+          </h2>
+          {/* divisor */}
+
+          <div className="h-[4px] w-[80px] bg-[#1F2A68]" />
+        </section>
+      </main>
+    </>
+  )
+}
--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -1,3 +1,23 @@
-export function ProductCard() {
-  return <div></div>
+import Image from 'next/image'
+import genericImg from '../../assets/productImage.png'
+
+interface ProductProps {
+  name: string
+  img?: string
+}
+
+export function ProductCard({ name, img }: ProductProps) {
+  return (
+    <div className="flex flex-col shadow-lg rounded-lg h-[300px] w-[200px]">
+      {/* Card com imagem */}
+      <div className="bg-slate-200 px-5 py-5">
+        <Image src={genericImg} alt="img do produto" />
+      </div>
+      <div className="px-6 py-3">
+        <span className="font-roboto text-base leading-[24px] font-medium">
+          {name}
+        </span>
+      </div>
+    </div>
+  )
 }
