8edb4520498b59a8850dff23fbfc925585a12c23
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-09-22 20:57:06 -0300

    update Page Product

--- src/app/(product)/product/page.tsx
+++ src/app/(product)/product/page.tsx
@@ -1,5 +1,7 @@
 import { AsideDesktop } from '@/components/home/header/AsideDesktop'
+import { DesktopLogo } from '@/components/home/header/DesktopLogo'
 import { Header } from '@/components/home/header/Header'
+import { ProductCard } from '@/components/product/ProductCard'
 
 export default function Product() {
   return (
@@ -11,13 +13,34 @@ export default function Product() {
         </div>
 
         {/* Container principal */}
-        <section className="flex flex-col gap-5 z-0 ml-[320px]">
-          <h2 className="font-roboto font-medium text-[#1F2A68] leading-[24px] text-lg md:text-[24px]">
-            Centro de furação
-          </h2>
-          {/* divisor */}
+        <section className="z-0 ml-[320px]">
+          {/* Desktop logo com separator */}
+          <DesktopLogo />
 
-          <div className="h-[4px] w-[80px] bg-[#1F2A68]" />
+          {/* Container Geral */}
+          <div className="px-20 py-10 flex flex-col gap-5">
+            <h2 className="font-roboto font-medium text-[#1F2A68] leading-[24px] text-lg md:text-[24px]">
+              Centro de furação
+            </h2>
+            {/* divisor */}
+
+            <div className="h-[4px] w-[80px] bg-[#1F2A68]" />
+
+            {/* Exibindo ... máquinas */}
+            <span className="text-[#9CA3AF] text-base leading-[24px] font-normal font-roboto">
+              Exibindo <strong className="text-black">06</strong> máquinas
+            </span>
+
+            {/* Div com os Cards */}
+            <div className="grid grid-cols-3 gap-5 py-5">
+              <ProductCard name="MSZ600AC" />
+              <ProductCard name="MSZ600AC" />
+              <ProductCard name="MSZ600AC" />
+              <ProductCard name="MSZ600AC" />
+              <ProductCard name="MSZ600AC" />
+              <ProductCard name="MSZ600AC" />
+            </div>
+          </div>
         </section>
       </main>
     </>
