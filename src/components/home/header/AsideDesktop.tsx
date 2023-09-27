b0c4fd994d0da5a649e063dbef043c170b5b7b04
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-09-27 17:23:12 -0300

    update header

--- src/components/home/header/AsideDesktop.tsx
+++ src/components/home/header/AsideDesktop.tsx
@@ -1,4 +1,4 @@
-import { Search } from 'lucide-react'
+import { Search, Home } from 'lucide-react'
 import { MenuItem } from './MenuItem'
 import { ProductSubMenu } from './CollapsibleSubMenus/Product'
 
@@ -6,11 +6,22 @@ export function AsideDesktop() {
   return (
     <aside className="hidden md:flex md:flex-col  w-[320px] border-r-[1px] border-[#1F2A68] ">
       {/* Search div */}{' '}
-      <div className="border-b bg-slate-100 cursor-pointer border-[#1F2A68] px-[25px] py-[30px] w-full flex items-center justify-center gap-[10px] h-[80px]">
-        <Search size={20} className="text-[#1F2A68]" />
-        <span className="uppercase text-base font-roboto font-medium leading-[20px] text-[#1F2A68]">
-          pesquisa
-        </span>
+      <div className="grid grid-cols-2">
+        {/* HomeNav Div */}
+        <div className="border-b bg-slate-100 cursor-pointer border-r border-[#1F2A68] px-[25px] py-[30px] w-full flex items-center justify-center gap-[10px] h-[80px]">
+          <Home size={20} className="text-[#1F2A68]" />
+          <span className="uppercase text-base font-roboto font-medium leading-[20px] text-[#1F2A68]">
+            Home
+          </span>
+        </div>
+
+        {/* Search div */}
+        <div className="border-b bg-slate-100 cursor-pointer border-[#1F2A68] px-[25px] py-[30px] w-full flex items-center justify-center gap-[10px] h-[80px]">
+          <Search size={20} className="text-[#1F2A68]" />
+          <span className="uppercase text-base font-roboto font-medium leading-[20px] text-[#1F2A68]">
+            pesquisa
+          </span>
+        </div>
       </div>
       <div className="flex flex-col bg-slate-100 h-screen">
         <ProductSubMenu />
