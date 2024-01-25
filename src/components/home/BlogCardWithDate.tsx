004ff60a4c5120d3a8b4439cc941d8377fa99070
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-01-25 14:17:47 -0300

    update width Skeleton and blogCardWithDate

--- src/components/home/BlogCardWithDate.tsx
+++ src/components/home/BlogCardWithDate.tsx
@@ -14,7 +14,7 @@ export function BlogCardWithDate({
   image,
 }: blogCardWithDateProps) {
   return (
-    <div className="shadow-lg flex flex-col rounded-md overflow-hidden md:w-[240px]">
+    <div className="shadow-lg flex flex-col rounded-md overflow-hidden md:w-[100%]">
       {/* container com texto, data e categoria */}
       <div className="p-5 bg-slate-100 gap-[72px] flex flex-col h-[224px] md:h-[224px]">
         <h3 className="font-medium text-sky-900 leading-6 md:line-clamp-2">
--- src/components/home/section9/Skeleton.tsx
+++ src/components/home/section9/Skeleton.tsx
@@ -1,5 +1,3 @@
 export function SkeletonSection9() {
-  return (
-    <div className="w-full md:w-[240px] h-[460px] bg-slate-200  animate-pulse"></div>
-  )
+  return <div className="w-full  h-[460px] bg-slate-200  animate-pulse"></div>
 }
