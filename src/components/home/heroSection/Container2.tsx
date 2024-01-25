31c903ed3d9cc4226a262d8c47cb3b33df5c27f9
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-01-25 14:26:39 -0300

    add video 2 to carousel

--- src/components/home/heroSection/Container2.tsx
+++ src/components/home/heroSection/Container2.tsx
@@ -1,10 +1,17 @@
-import Image from 'next/image'
-import containerImage from '../../../assets/heroImgBg.jpg'
-
 export function Container2() {
+  // Video Container
   return (
-    <div className="w-full aspect-video md:flex md:items-start ">
-      <Image src={containerImage} alt="Texto alternativo" className="h-full" />
+    <div className="w-full aspect-video max-h-[480px] overflow-hidden md:flex md:items-center">
+      <video
+        className="w-full h-auto"
+        autoPlay // Configurar autoplay para o vídeo
+        controls={false} // Remover os controles do vídeo
+        loop // Opcional: configurar o loop do vídeo
+        muted // Opcional: desativar o som do vídeo (se necessário)
+      >
+        <source src="/video2cnc.MOV" type="video/mp4" />
+        Seu navegador não suporta o elemento de vídeo.
+      </video>
     </div>
   )
 }
